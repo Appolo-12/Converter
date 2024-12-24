149fbac4fdca36c6c8e37b92951e5037542e10eb
Appolo-13 <rayl1x.gta@gmail.com>
2024-12-24 17:54:25 +0100

    Changed description and added conversion from miles to kilometers.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("This project will convert km/h to mph.");
+    private static final double CONVERT_KMH = 0.62137119;
 
+    public static void main(String[] args) {
+        System.out.println("This project will convert km/h to mph and vice versa.");
+
+        double mph = 1;
+        double convertedMiles = convertMPHToKm(mph);
+
+        System.out.println("Miles without conversion to kilometers: " + mph + "\n" +
+                "Miles with conversion to kilometers: " + convertedMiles);
+    }
+
+    private static double convertMPHToKm(double mph) {
+        return mph / CONVERT_KMH;
     }
 }
